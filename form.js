776f1443047a4d72e6076c3c0824e6b1b94f54a7
LuchoProgramer <luchoviteri1990@gmail.com>
2024-08-03 17:36:45 -0500

    ahora si

--- form.js
+++ form.js
@@ -1,9 +1,8 @@
-// Si usas un módulo importado
-import emailjs from 'emailjs-com';
-
-emailjs.init('jgAwVR-flCTKQnK_X');
+import emailjs from 'https://cdn.jsdelivr.net/npm/emailjs-com@3.2.0/dist/email.min.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+    emailjs.init('jgAwVR-flCTKQnK_X');
+
     const form = document.getElementById('contact-form');
 
     form.addEventListener('submit', function(event) {
